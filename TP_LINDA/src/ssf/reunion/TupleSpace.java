af8253c8feb6d765c4d1e0d639365a8cddde2ed9
sebastien tolron <sebastien.tolron@gmail.com>
2013-04-15 15:54:18 +0200

    Ajout de la classe TupleSpace

---
+++ TP_LINDA/src/ssf/reunion/TupleSpace.java
@@ -0,0 +1,53 @@
+package ssf.reunion;
+
+/**
+* TP 1 Introduction a larchitecture Linda
+* @author Sebastien Tolron sebastien.tolron@etu.univ-savoie.fr>
+* @author Samuel De Francesco <samuel.defrancesco@etu.univ-savoie.fr>
+* @author François Profizi <francois.profizi@etu.univ-savoie.fr>
+*/
+
+
+public class TupleSpace {
+
+/* Declaration des methodes */
+	
+/** 
+ * Méthode rd qui permet de lire une valeur dans un espace de tuple
+*  @param key La clé du tuple à consommer
+*  @return La valeur du tuple
+*/
+	
+	public void rd (){
+		
+	}	
+	
+	
+/** 
+ * Méthode in qui permet de consommer un tuple dans un espace de tuple
+*  @param key La clé du tuple à consommer
+*  @return La valeur du tuple
+*/
+	
+	
+	public void in (){
+		
+	}
+
+	
+/** 
+ * Méthode out qui permet d'ajouter un tuple à l'espace de tuple
+*  @param key La clé du tuple à consommer
+*  @return La valeur du tuple
+*/
+	
+	
+	public void out (){
+		
+	}
+	
+
+
+	
+	
+}
